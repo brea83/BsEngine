dd2d6a57b77c6df58be041b385a996b7e9a49403
Briana <brea83@gmail.com>
2025-10-28 11:07:34 +0100

    following along with learn OpenGL's "hello window" tutorial
     old bad triangle is gone. window is now resizable. preety sure we're going to abstract some of this user input stuff out later, but for now it's working.

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -4,6 +4,12 @@
 #include <GLFW/glfw3.h>
 // MID GREEN DESATURATED (0.4, 0.5, 0.2, 1)
 
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
 int main()
 {
 	GLFWwindow* window;
@@ -11,44 +17,66 @@ int main()
 
 	if (!glfwInit())
 	{
+		std::cout << "Failed to init GLFW" << std::endl;
 		return -1;
 	}
+	// window settings set up as done in Learn OpenGl's tutorial
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// create a windowed mode window and its openGL context
-
-	window = glfwCreateWindow(640, 480, "BsEngine V1", NULL, NULL);
+	window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "BsEngine V1", NULL, NULL);
 	if (!window)
 	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+
 		glfwTerminate();
 		return -1;
 	}
-
 	//make the windows's current context current
 	glfwMakeContextCurrent(window);
+	// setting up window resize callback
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// initialize GLAD to load openGL function pointers
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
 
 	// loop until the user closes window
 	while (!glfwWindowShouldClose(window))
 	{
-		//render
+		// input
+		processInput(window);
+
+		// render
+		glClearColor(0.2f, 0.1f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-		glClearColor(0.4, 0.5, 0.2, 1);
 
-		// messy oldfashiond bad way of rendering
-		/*glBegin(GL_TRIANGLES);
-
-		glVertex2f(0.5f, -0.5f);
-		glVertex2f(0.0f, 0.5f);
-		glVertex2f(-0.5f, -0.5f);
-
-		glEnd();*/
-
-		//swap front and back buffers
+		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		glfwSwapBuffers(window);
-
-		// poll for and process events CANT CLOSE THE WINDOW WITHOUT THIS
 		glfwPollEvents();
 	}
 
 	glfwTerminate();
 	return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
 }

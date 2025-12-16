dac1ec364c62cb791b19531f9213913740e6c05f
Briana <brea83@gmail.com>
2025-12-16 19:22:55 +0100

    tweaked light model a little

--- Assets/Shaders/FragmentShader.glsl
+++ Assets/Shaders/FragmentShader.glsl
@@ -73,16 +73,17 @@ void main()
 	if(bUseMainLight)
 	{
 		
+		FragColor.xyz += ambientLight.xyz * textureColor;
 		vec3 N = normalize(Normal);
 		float nDotL = max(dot(N, MainLight.direction), 0.0);
 		vec3 V = normalize(cameraPosition.xyz - WorldPos);
 
 		if(nDotL > 0)
-			{
-			float distance = length(lightPosition - WorldPos);
-			attenuation = 1.0 / (MainLight.attenuation.x + (MainLight.attenuation.y * distance) + (MainLight.attenuation.z * pow(distance, 2)));
-
-			diffuse = nDotL * MainLight.color * attenuation;
+		{
+//			float distance = length(lightPosition - WorldPos);
+//			attenuation = 1.0 / (MainLight.attenuation.x + (MainLight.attenuation.y * distance) + (MainLight.attenuation.z * pow(distance, 2)));
+			// until I start doing more pbr like stuff assume the diffuse intensity is just a little less than full reflection
+			diffuse = nDotL * MainLight.color * 0.8;
 
 		//specular
 			vec3 H = normalize((MainLight.direction + V));
@@ -92,11 +93,12 @@ void main()
 			specBrightness = pow(max(specBrightness, 0.0), material.specularPower);
 
 			// until I start doing more pbr assume light intensity on spec is about a third of the main light color
-			specular = specBrightness * (MainLight.color * 0.3) * attenuation;
+			specular = specBrightness * (MainLight.color * 0.3);
 
+			FragColor.xyz += (diffuse + specular) * textureColor; 
 		}
-			FragColor.xyz += (ambientLight.xyz + diffuse + specular) * textureColor; 
-		}
+
+	}
 	else
 	{
 		FragColor.xyz = textureColor;
